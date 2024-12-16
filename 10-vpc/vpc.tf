c88d8fbf822567b33571fbd2a3f4aceea7e3c316
Venkata <venkataraju.bandaru8@gmail.com>
2024-12-16 06:27:43 +0530

    vpc

--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../../vpc-module-terraform"
+    #source = "../../vpc-module-terraform"
+    source = "git::https://github.com/venkatarajub/vpc-module-terraform.git"
     project = var.project
     environment = var.environment
     public_cidrs = var.public_cidrs
