f98980e0ec6e950703b4c5f20a513e0cd0dda30b
Venkata <venkataraju.bandaru8@gmail.com>
2024-12-18 06:59:28 +0530

    sg

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -47,3 +47,102 @@ module "ansible_sg" {
     common_tags = var.common_tags
     sg_tags = var.ansible_sg_tags
 }
+
+# we are not mentionned ay ingress values in the module. To provide ingress values here we are writing sg rules
+resource "aws_security_group_rule" "mysql-backend" {
+    type = "ingress"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    source_security_group_id = module.backend_sg.id 
+    security_group_id = module.mysql_sg.id
+}
+
+resource "aws_security_group_rule" "backend-frotend" {
+    type = "ingress"
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    source_security_group_id = module.frontend_sg.id 
+    security_group_id = module.backend_sg.id 
+}
+resource "aws_security_group_rule" "frotend-public" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.backend_sg.id 
+}
+
+resource "aws_security_group_rule" "mysql-bastion" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.bastion_sg.id 
+    security_group_id = module.mysql_sg.id
+}
+
+resource "aws_security_group_rule" "backend-bastion" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.bastion_sg.id 
+    security_group_id = module.backend_sg.id 
+}
+
+resource "aws_security_group_rule" "frotend-bastion" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.bastion_sg.id 
+    security_group_id = module.frontend_sg.id 
+}
+
+resource "aws_security_group_rule" "mysql-ansible" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.ansible_sg.id 
+    security_group_id = module.mysql_sg.id
+}
+
+resource "aws_security_group_rule" "backend-ansible" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.ansible_sg.id 
+    security_group_id = module.backend_sg.id 
+}
+
+resource "aws_security_group_rule" "frotend-ansible" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    source_security_group_id = module.ansible_sg.id 
+    security_group_id = module.frontend_sg.id 
+}
+
+resource "aws_security_group_rule" "bastion-public" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.bastion_sg.id
+}
+
+resource "aws_security_group_rule" "ansible-public" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = module.ansible_sg.id
+}
